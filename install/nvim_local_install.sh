bc12058df1cb8f8bcc5aa3fc8e49a16e12132fc8
soummyam <soummyam@gmail.com>
2022-12-21 17:27:11 -0800

    fixed nvim install procedure

--- install/nvim_local_install.sh
+++ install/nvim_local_install.sh
@@ -1,2 +1,3 @@
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-realpath build/bin/nvim
+https://github.com/neovim/neovim/releases
+wget https://github.com/neovim/neovim/releases/download/v0.8.1/nvim.appimage
+chmod u+x nvim.appimage && mv ./nvim.appimage $HOME/.local/bin
