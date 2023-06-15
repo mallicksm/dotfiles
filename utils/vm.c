3436c6dba91c23b5d3ad75aae47d4c0ec40b15ea
soummya <soummya@aeva.ai>
2023-06-15 10:38:17 -0700

    all TG works, 12, 14 and 16

--- utils/vm.c
+++ utils/vm.c
@@ -5,25 +5,24 @@
 #define uint32 unsigned int
 #define uint64 unsigned long long
 
-#define TG 12                                                // 12|14|16
-#define MAXVA            ((TG) + 3*((TG)-3))                 // 4K=[9+9+9+12], 16K=[11+11+11+14], 64K=[13+13+13+16]
+#define TG 12                                             // 12|14|16
+#define MAXVA            ((TG) + 3*((TG)-3))              // 4K=[9+9+9+12], 16K=[11+11+11+14], 64K=[13+13+13+16]
 
-#define PGSIZE           (1 << (TG))                         // 4K=0x1000, 16K=0x4000, 64K=0x10000
-#define PXMASK           ((1 << (TG-3)) - 1)                 // 4K=0x1ff, 16K=0x7ff, 64K=0x1fff
-#define PXSHIFT(level)   (TG + ((TG-3) * (3 - level)))       // 4K=[12 + 9*(3-level)], 16K=[14 + 11*(3-level)], 64K=[16 + 13*(3-level)]
+#define PGSIZE           (1 << (TG))                      // 4K=0x1000, 16K=0x4000, 64K=0x10000
+#define PXMASK           ((1 << ((TG)-3)) - 1)            // 4K=0x1ff, 16K=0x7ff, 64K=0x1fff
+#define PXSHIFT(level)   (TG + (((TG)-3) * (3 - level)))  // 4K=[12 + 9*(3-level)], 16K=[14 + 11*(3-level)], 64K=[16 + 13*(3-level)]
 
-#define PX(level, va)    ((va >> PXSHIFT(level)) & PXMASK)
-#define PTE2PA(pte)      (pte & ~(PGSIZE-1))
-#define PA2PTE(pa)       (pa & ~(PGSIZE-1))
+#define PX(level, va)    (((va) >> PXSHIFT(level)) & PXMASK)
+#define PAxPTE(a)        ((a) & ~(PGSIZE-1))
 
-#define LPGSIZE(l)       (1 << ((TG)+((TG)-3)*(3-(l))))      // 4K=[9,9,9,12], 16K=[11,11,11,14], 64K=[13,13,13,16]
+#define LPGSIZE(l)       (1 << ((TG)+((TG)-3)*(3-(l))))   // 4K=[9,9,9,12], 16K=[11,11,11,14], 64K=[13,13,13,16]
 #define PGROUNDDOWN(a,l) ((a) & ~(LPGSIZE(l) - 1))
 #define PGROUNDUP(a,l)   (((a) + LPGSIZE(l) - 1) & ~(LPGSIZE(l) - 1))
 
 #define PTE_V (0x1ULL << 0)
 #define PTE_T (0x1ULL << 1)
 
-__attribute((aligned(4096))) uint64 pagetable[512 * 10];
+__attribute((aligned(PGSIZE))) uint64 pagetable[(PGSIZE>>3) * 10];
 int numpages = 1;
 
 void logprintf(char *fmt, ...) {
@@ -34,7 +33,7 @@ void logprintf(char *fmt, ...) {
 }
 
 uint64 *kalloc(void) {
-   return &pagetable[512 * numpages++];
+   return &pagetable[(PGSIZE>>3) * numpages++];
 }
 
 void pteprint(uint64 *pagetable, int level) {
@@ -50,7 +49,7 @@ void pteprint(uint64 *pagetable, int level) {
       for (int i = 0; i < PXMASK+1; i++) {
          if ((pagetable[i] & PTE_T) && (pagetable[i] & PTE_V)) {
             printf("%*s%d: pte=0x%016llx\n", (level-1)*_PINDENT, "", i, pagetable[i]);
-            nextLevelTable = (uint64 *)PTE2PA(pagetable[i]);
+            nextLevelTable = (uint64 *)PAxPTE(pagetable[i]);
             pteprint(nextLevelTable, level + 1);
          } else if (pagetable[i] & PTE_V) {
             printf("%*s%d: blk=0x%016llx\n", (level-1)*_PINDENT, "", i, pagetable[i]);
@@ -67,17 +66,17 @@ uint64 *walk(uint64 *pagetable, uint64 va, uint32 levels, int alloc) {
 
       uint64 *pte = &pagetable[PX(level,va)];
       if ((*pte & PTE_T) && (*pte & PTE_V)) {
-         pagetable = (uint64 *)PTE2PA(*pte);
-         logprintf("found table=%d numpages=%d &pte=0x%016llx pte=0x%016llx\n", level, numpages,pte,*pte);
+         pagetable = (uint64 *)PAxPTE(*pte);
+         logprintf("found table=%d numpages=%d pt=0x%016llx pte=0x%016llx\n", level, numpages,pte,*pte);
       } else if (*pte & PTE_V) {
-         pagetable = (uint64 *)PTE2PA(*pte);
-         logprintf("found block=%d numpages=%d &pte=0x%016llx pte=0x%016llx\n", level, numpages,pte,*pte);
+         pagetable = (uint64 *)PAxPTE(*pte);
+         logprintf("found block=%d numpages=%d pt=0x%016llx pte=0x%016llx\n", level, numpages,pte,*pte);
          return pagetable;
       } else {
          if (!alloc || (pagetable = kalloc()) == 0)
             return 0;
-         *pte = PA2PTE((uint64)pagetable) | PTE_T | PTE_V;
-         logprintf("alloc table=%d numpages=%d &pte=0x%016llx pte=0x%016llx\n", level, numpages,pte,*pte);
+         *pte = PAxPTE((uint64)pagetable) | PTE_T | PTE_V;
+         logprintf("alloc table=%d numpages=%d pt=0x%016llx pte=0x%016llx\n", level, numpages,pte,*pte);
       }
    }
 done:
@@ -98,9 +97,9 @@ int mappages(uint64 *pagetable, uint64 va, uint64 pa, uint32 level, uint64 size,
          return -1;
       }
       if (level == 3) {
-         *pte = PA2PTE(pa) | perm | PTE_T | PTE_V;
+         *pte = PAxPTE(pa) | perm | PTE_T | PTE_V;
       } else {
-         *pte = PA2PTE(pa) | perm | PTE_V;
+         *pte = PAxPTE(pa) | perm | PTE_V;
       }
       if (a == last) {
          goto done;
@@ -141,7 +140,7 @@ int mappages_cli(int argc, char** argv) {
       pages = strtol(argv[2], NULL, 0);
       levels = strtol(argv[3], NULL, 0);
    }
-   mappages(pagetable, va, (0x123456789a000ULL), levels, pages*4096, 0x5bULL<<(14*4));
+   mappages(pagetable, va, 0x123456789a000ULL+va, levels, pages*PGSIZE, 0x5bULL<<(14*4));
    return 0;
 }
 
