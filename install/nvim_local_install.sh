1c511289664be1344c32d6c835ce4a8984c1cfdc
soummya <soummya@aeva.ai>
2023-08-17 20:13:57 -0700

    refreshed install scripts

--- install/nvim_local_install.sh
+++ install/nvim_local_install.sh
@@ -1,3 +1,3 @@
 https://github.com/neovim/neovim/releases
-wget https://github.com/neovim/neovim/releases/download/v0.8.1/nvim.appimage
-chmod u+x nvim.appimage && mv ./nvim.appimage $HOME/.local/bin
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage && mv ./nvim.appimage $HOME/.local/bin/nvim
--- install/vim_local_install.sh
+++ install/vim_local_install.sh
@@ -1,7 +1,14 @@
 # install yum install *GTK3* *X11* *Xmu* *builddep*
 ./configure --prefix=$HOME/.local \
-   --with-features=huge --enable-multibyte \
+   --with-features=huge \
+   --enable-multibyte \
    --with-python3-command=python3.6  \
-   --enable-rubyinterp --enable-pythoninterp --enable-luainterp --enable-gui=auto \
+   --enable-rubyinterp \
+   --enable-pythoninterp \
+   --enable-luainterp \
+   --enable-gui=gtk2 \
    --enable-python3interp=yes  \
-   --enable-gui=gtk3
+   --enable-cscope
+#make 
+mkdir -p $HOME/.local
+#make install
