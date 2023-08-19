f7e0b08c0c0c3ca4b3f8c97f845d1f79c1ce180d
soummya <soummya@aeva.ai>
2023-08-18 19:04:26 -0700

    finished coloring tmux statusbar

--- utils/tmux_status-right.sh
+++ utils/tmux_status-right.sh
@@ -2,11 +2,23 @@ prompt_git() {
    local s='';
    local branchName='';
    local gitStatus='';
-   local c_style=#[fg='colour204'];
+   local dollar='$';
+   local dateTime=$(date +"%a %b %d %Y, %I:%M %p");
+
+   local c_hostname=#[fg='red'];
+   local c_branch=#[fg='colour204'];
+   local c_datetime=#[fg='blue'];
+
+   local c_style=#[fg='yellow'];
+   local c_deleted=#[fg='red'];
+   local c_modified=#[fg='yellow'];
+   local c_untracked=#[fg='yellow'];
    local c_staged=#[fg='green'];
-   local c_deleted='red';
-   local c_ahead='green';
-   local c_behind='yellow';
+   local c_up_to_date=#[fg='yellow'];
+   local c_ahead=#[fg='green'];
+   local c_diverged=#[fg='yellow'];
+   local c_behind=#[fg='yellow'];
+
    local c_default=#[fg=default];
 
    # Check if the current directory is in a Git repository.
@@ -30,38 +42,38 @@ prompt_git() {
 
          # Check for deleted changes in the index.
          if [[ $deleted > 0 ]]; then
-            s+="$c_deleted✘${deleted}$c_default";
+            s+="${c_deleted}✘${deleted}${c_default}";
          fi
 
          # Check for modified changes in the index.
          if [[ $modified > 0 ]]; then
-            s+="#[fg=yellow]${modified}$c_default";
+            s+="${c_modified}${modified}${c_default}";
          fi
 
          # Check for staged changes in the index.
          if [[ $staged > 0 ]]; then
-            s+="${c_staged}+${staged}$c_default";
+            s+="${c_staged}+${staged}${c_default}";
          fi
 
          # Check for untracked changes in the index.
          if [[ $untracked > 0 ]]; then
-            s+="${untracked}";
+            s+="${c_untracked}${untracked}${c_default}";
          fi
 
          # Check for stashed files.
          if git rev-parse --verify refs/stash &>/dev/null; then
-            s+='$';
+            s+="${c_style}${dollar}${c_default}";
          fi;
 
          ahead_behind=$(git status -sb)
          behind=$(echo "$ahead_behind" |grep behind| sed -E 's/.*\[behind (.)+\]/\1/')
-         ahead=$(echo "$ahead_behind"  |grep ahead|  sed -E 's/.*\[ahead (.)+\]/\1/')
+         ahead=$(echo "$ahead_behind"  |grep ahead | sed -E 's/.*\[ahead (.)+\]/\1/')
          if [[ $ahead > 0 ]]; then
-            s+="⇡${ahead}";
+            s+="$c_ahead⇡${ahead}$c_default";
          elif [[ $behind > 0 ]]; then
-            s+="⇣${behind}";
+            s+="$c_behind⇣${behind}$c_default";
          else
-            s+="✓";
+            s+="${c_up_to_date}✓${c_default}";
          fi
       fi;
 
@@ -72,13 +84,15 @@ prompt_git() {
          git rev-parse --short HEAD 2> /dev/null || \
          echo '(unknown)')";
 
-      [ -n "${s}" ] && s=" [${s}]";
+      [ -n "${s}" ] && s=" ${c_style}[${c_default}${s}${c_style}]${c_default}";
 
-      gitStatus="$c_style[ ${branchName}]$c_default${s}";
+      gitStatus="$c_branch[ ${branchName}]$c_default${s}";
    else
       gitStatus=""
    fi;
-   echo -e "#[fg=red] $(hostname -s): $gitStatus"
+   dateTimeStatus="${c_dateTime}$dateTime${c_default}"
+   hostnameStatus="${c_hostname}$(hostname -s):${c_default}"
+   echo -e "$dateTimeStatus $hostnameStatus $gitStatus"
 }
 
 prompt_git
