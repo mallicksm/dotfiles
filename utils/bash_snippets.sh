df00b18060168e5e39f5b8d9066f2d99100cc8bb
soummya <soummya@aeva.ai>
2023-08-29 13:58:29 -0700

    removed utils.sh from install and put wget4me in bash_snippets.sh

--- utils/bash_snippets.sh
+++ utils/bash_snippets.sh
@@ -110,3 +110,17 @@ function download() {
    error "Command failed (exit code $rc): ${BLUE}${cmd}${NO_COLOR}"
    return $rc
 }
+function wget4me() {
+    urltype=$1
+    url=$2
+
+    if [[ $urltype == "tar" ]]; then
+       tarfile=$(basename $url)
+       if [[ ! -e $tarfile ]]; then
+          echo "Info: wgetting $url"
+          wget $url --no-check-certificate > $tarfile.wget.log 2>&1
+          echo "Info: expanding $tarfile"
+          tar -xvf $tarfile > $tarfile.tar.log 2>&1
+       fi
+    fi
+}
