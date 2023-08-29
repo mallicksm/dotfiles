714be9e4024785fb45da75857a351c9cbed57d9b
soummya <soummya@aeva.ai>
2023-08-29 14:02:44 -0700

    format change only

--- utils/bash_snippets.sh
+++ utils/bash_snippets.sh
@@ -111,16 +111,16 @@ function download() {
    return $rc
 }
 function wget4me() {
-    urltype=$1
-    url=$2
+   urltype=$1
+   url=$2
 
-    if [[ $urltype == "tar" ]]; then
-       tarfile=$(basename $url)
-       if [[ ! -e $tarfile ]]; then
-          echo "Info: wgetting $url"
-          wget $url --no-check-certificate > $tarfile.wget.log 2>&1
-          echo "Info: expanding $tarfile"
-          tar -xvf $tarfile > $tarfile.tar.log 2>&1
-       fi
-    fi
+   if [[ $urltype == "tar" ]]; then
+      tarfile=$(basename $url)
+      if [[ ! -e $tarfile ]]; then
+         echo "Info: wgetting $url"
+         wget $url --no-check-certificate > $tarfile.wget.log 2>&1
+         echo "Info: expanding $tarfile"
+         tar -xvf $tarfile > $tarfile.tar.log 2>&1
+      fi
+   fi
 }
