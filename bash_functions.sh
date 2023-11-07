d3901bc33793f6e07b1b27436a8a2a8927de7e4a
soummya <soummya@aeva.ai>
2023-11-07 08:56:51 -0800

    added automation to dis and fixed ctrl-t to editor

--- bash_functions.sh
+++ bash_functions.sh
@@ -271,6 +271,7 @@ export FZF_CTRL_T_OPTS="
    --height 100% 
    --preview 'bat -n --theme=Nord --color=always {}'
    --bind 'ctrl-/:change-preview-window(down|hidden|)'
+   --bind 'enter:become(vim {} < /dev/tty > /dev/tty)'
    --color header:italic
    --header 'Press CTRL-/ to toggle preview'"
 
@@ -427,6 +428,10 @@ function num {
 }
 function dis() {
    input="$@"
+   ARM_COMPILER=$(suite_exec --help|grep '^Arm Compiler.*6')
+   export ARMLMD_LICENSE_FILE=8224@license01
+   export ARM_PRODUCT_DEF=/opt/tools/arm/developmentstudio-2020.1-1/sw/ARMCompiler5.06u7/sw/mappings/gold.elmap
+
    echo "quit to exit"
    if [[ $input =~ ^(-h|help)$ ]]; then
       echo "Usage:"
@@ -437,13 +442,13 @@ function dis() {
    fi
    if [[ $input =~ ^(-v7)$ ]]; then
       ARCH="ARM-v7"
-      CC=(suite_exec -q -t 'Arm Compiler 6' armclang --target=arm-arm-none-eabi -marm -mcpu=cortex-r5)
-      LK=(suite_exec -q -t 'Arm Compiler 6' armlink --diag_suppress=L6305W)
+      CC=(suite_exec -q -t "$ARM_COMPILER" armclang --target=arm-arm-none-eabi -marm -mcpu=cortex-r5)
+      LK=(suite_exec -q -t "$ARM_COMPILER" armlink --diag_suppress=L6305W)
       OBJDUMP=(aarch64-unknown-nto-qnx7.1.0-objdump)
    else
       ARCH="ARM-v8"
-      CC=(suite_exec -q -t 'Arm Compiler 6' armclang --target=aarch64-arm-none-eabi)
-      LK=(suite_exec -q -t 'Arm Compiler 6' armlink --diag_suppress=L6305W)
+      CC=(suite_exec -q -t "$ARM_COMPILER" armclang --target=aarch64-arm-none-eabi)
+      LK=(suite_exec -q -t "$ARM_COMPILER" armlink --diag_suppress=L6305W)
       OBJDUMP=(aarch64-unknown-nto-qnx7.1.0-objdump)
    fi
    while true; do
