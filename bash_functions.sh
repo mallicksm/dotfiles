21390b3d60488a6398990ce32231ddcff3039a48
soummya <soummya@aeva.ai>
2024-01-08 08:33:22 -0800

    fixed race condition in bash_utils

--- bash_functions.sh
+++ bash_functions.sh
@@ -9,6 +9,35 @@
 # Description: basic functions for shell
 #
 #===============================================================================
+function modpath () {
+   loc=$1
+   cmd=${2:-"d"}
+   var=${3:-PATH}
+   eval PATHv=\$$var    # set PATHv to the var reference (double indirection)
+   NEW_PATH=${PATHv/#"$loc:"}         #    Begining
+   NEW_PATH=${NEW_PATH/#"$loc"}       #    Solo
+   NEW_PATH=${NEW_PATH/%":$loc"}      #    Ending
+   NEW_PATH=${NEW_PATH//":$loc:"/:}   #    Multiple middles
+
+   if [ $cmd = "d" ]; then
+      export ${var}=$NEW_PATH
+   elif [ $cmd = "b" ]; then
+      if [ -z $NEW_PATH ]; then
+         export ${var}=$loc   # old PATH empty
+      else
+         export ${var}=$loc:$NEW_PATH
+      fi
+   elif [ $cmd = "a" ]; then
+      if [ -z $NEW_PATH ]; then
+         export ${var}=$loc   # old PATH empty
+      else
+         export ${var}=$NEW_PATH:$loc
+      fi
+   else
+      echo "usage: modpath [location] [d elete|b efore|a fter] [VARIABLE]"
+   fi
+}
+modpath ~/.local/bin b
 for file in $(command ls ~/dotfiles/utils/bash_*.sh); do
    source $file
 done
--- utils/bash_snippets.sh
+++ utils/bash_snippets.sh
@@ -65,34 +65,6 @@ function print_sequence() {
    done
    echo
 }
-function modpath () {
-   loc=$1
-   cmd=${2:-"d"}
-   var=${3:-PATH}
-   eval PATHv=\$$var    # set PATHv to the var reference (double indirection)
-   NEW_PATH=${PATHv/#"$loc:"}         #    Begining
-   NEW_PATH=${NEW_PATH/#"$loc"}       #    Solo
-   NEW_PATH=${NEW_PATH/%":$loc"}      #    Ending
-   NEW_PATH=${NEW_PATH//":$loc:"/:}   #    Multiple middles
-
-   if [ $cmd = "d" ]; then
-      export ${var}=$NEW_PATH
-   elif [ $cmd = "b" ]; then
-      if [ -z $NEW_PATH ]; then
-         export ${var}=$loc   # old PATH empty
-      else
-         export ${var}=$loc:$NEW_PATH
-      fi
-   elif [ $cmd = "a" ]; then
-      if [ -z $NEW_PATH ]; then
-         export ${var}=$loc   # old PATH empty
-      else
-         export ${var}=$NEW_PATH:$loc
-      fi
-   else
-      echo "usage: modpath [location] [d elete|b efore|a fter] [VARIABLE]"
-   fi
-}
 function var () {
    VAR=${1:-PATH}
    DOLLARVAR=${!VAR}
