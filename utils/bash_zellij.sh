73aedeb139d081797dc3da7de0575b9ec08463d2
soummya <soummya@aeva.ai>
2024-02-29 10:11:00 -0800

    added zm

--- utils/bash_zellij.sh
+++ utils/bash_zellij.sh
@@ -1,2 +1,5 @@
 export ZELLIJ_CONFIG_DIR=~/dotfiles/initrc/zellij
+function zm() {
+   session=$(zellij ls 2>/dev/null | fzf --exit-0 --ansi) && zellij attach "${session%% *}" || echo "No sessions found."
+}
 
