b9d0e7660c6be7cd2186cdbc2031451fe3cbc808
soummya <soummya@aeva.ai>
2024-03-02 08:11:47 -0800

    added zellij aliases like zr zrf

--- utils/bash_zellij.sh
+++ utils/bash_zellij.sh
@@ -2,4 +2,15 @@ export ZELLIJ_CONFIG_DIR=~/dotfiles/initrc/zellij
 function zm() {
    session=$(zellij ls 2>/dev/null | fzf --exit-0 --ansi) && zellij attach "${session%% *}" || echo "No sessions found."
 }
+function zr () { 
+   zellij run --name "$*" -- bash -ic "$*";}
+function zrf () { 
+   zellij run --name "$*" --floating -- bash -ic "$*";
+}
+function ze () { 
+   zellij edit "$*";
+}
+function zef () { 
+   zellij edit --floating "$*";
+}
 
