7290142ab7bae2006908780127a5f655331da83a
soummya <soummya@aeva.ai>
2024-03-03 12:00:50 -0800

    added zm

--- utils/bash_zellij.sh
+++ utils/bash_zellij.sh
@@ -1,10 +1,67 @@
 export ZELLIJ_CONFIG_DIR=~/dotfiles/initrc/zellij
+function common_options () {
+   case $1 in
+      -h|--help|-help|help)
+         opt[HELP]=1
+         ;;
+      -*)
+         echo "Attention: Unknown Argument $1"
+         return 1
+         ;;
+      *)
+         args+=("$1")
+         ;;
+   esac
+}
+declare -A helpstr=()
+helpstr["zm"]="\
+   usage: zm [options|-h]
+   create or attach to zellij session
+
+      ------
+      Options:
+      -s|--create-session <session-name>         -Create a session with <session-name>
+      -k|--kill-all-sessions                     -kill all sessions
+"
 function zm () {
    if [[ ! -z $ZELLIJ ]]; then
       echo "Attention: cannot be run from within zellij"
       return
    fi
-   session=$(zellij ls 2>/dev/null | fzf --exit-0 --ansi --header 'Enter to open:') && zellij attach "${session%% *}"
+   declare -A opt=()
+   args=()
+   while (( $# )); do
+      case $1 in 
+         -s|--create-session)
+            opt[ZELLIJ_SESSION_NAME]="$2"
+            shift 2
+         ;;
+         -k|--kill-all-sessions)
+            opt[ZELLIJ_KILL_ALL_SESSIONS]="yes"
+            shift 1
+         ;;
+         *) common_options "$1";shift 1
+      esac
+   done
+   [[ "${opt[HELP]}" ]] && { echo "${helpstr[$FUNCNAME]}";return 0; }
+   num_sessions=$(zellij ls 2>/dev/null | wc -l)
+   if [[ ${opt[ZELLIJ_KILL_ALL_SESSIONS]} == "yes" ]]; then
+      zellij ka -y
+      zellij da -y
+      rm -rf ~/.cache/zellij/
+      return
+   fi
+   if [[ $num_sessions -eq 0 ]]; then
+      if [[ -z ${opt[ZELLIJ_SESSION_NAME]} ]]; then
+         echo "Attention: no sessions found or new name given"
+      else
+         zellij -s ${opt[ZELLIJ_SESSION_NAME]}
+      fi
+   elif [[ ($num_sessions -eq 1) && ( ! $(zellij ls) =~ EXITED ) ]]; then
+      zellij attach --create
+   else
+      session=$(zellij ls 2>/dev/null | fzf --exit-0 --ansi --header 'Enter to open:') && zellij attach "${session%% *}" || echo "No sessions found"
+   fi
 }
 function zr () { 
    zellij run --name "$*" -- bash -ic "$*";}
