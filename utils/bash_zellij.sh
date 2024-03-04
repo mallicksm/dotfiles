9d38bd7a4a7015da4dd163fcef4667cfbf21f160
soummya <soummya@aeva.ai>
2024-03-03 18:43:29 -0800

    works from within zellij

--- utils/bash_zellij.sh
+++ utils/bash_zellij.sh
@@ -20,14 +20,10 @@ helpstr["zm"]="\
 
       ------
       Options:
-      -s|--create-session <session-name>         -Create a session with <session-name>
-      -k|--kill-all-sessions                     -kill all sessions
+      -s|--create-session <session-name>         -Create a session with <session-name> (Not within zellij)
+      -k|--kill-all-sessions                     -kill all sessions (Not within zellij)
 "
 function zm () {
-   if [[ ! -z $ZELLIJ ]]; then
-      echo "Attention: cannot be run from within zellij"
-      return
-   fi
    declare -A opt=()
    args=()
    while (( $# )); do
@@ -44,6 +40,11 @@ function zm () {
       esac
    done
    [[ "${opt[HELP]}" ]] && { echo "${helpstr[$FUNCNAME]}";return 0; }
+   if [[ ! -z $ZELLIJ ]]; then
+      # working within zellij
+      zellij action launch-or-focus-plugin zellij:session-manager --floating
+      return
+   fi
    num_sessions=$(zellij ls 2>/dev/null | wc -l)
    if [[ ${opt[ZELLIJ_KILL_ALL_SESSIONS]} == "yes" ]]; then
       zellij ka -y
@@ -53,21 +54,19 @@ function zm () {
       return
    fi
    if [[ ! -z ${opt[ZELLIJ_SESSION_NAME]} ]]; then
+      # user gave a new name, must create session
       zellij --session ${opt[ZELLIJ_SESSION_NAME]} 
       return
    fi
-   if [[ $num_sessions -eq 0 ]]; then
-      if [[ -z ${opt[ZELLIJ_SESSION_NAME]} ]]; then
-         echo "Attention: no sessions found or new name given"
-      else
-         zellij --session ${opt[ZELLIJ_SESSION_NAME]}
-      fi
+   if [[ ($num_sessions -eq 0) && ( -z ${opt[ZELLIJ_SESSION_NAME]} ) ]]; then
+      # no sessions user must provide name
+      echo "Attention: no sessions found or new name given"
    elif [[ ($num_sessions -eq 1) && ( ! $(zellij ls) =~ EXITED ) ]]; then
       # Only attach if a single good session exists
       zellij attach --create
    else
       # fzf if >1 session exists (even if EXITED)
-      session=$(zellij ls 2>/dev/null | fzf --exit-0 --ansi --header 'Enter to open:') && zellij attach "${session%% *}" || echo "No sessions selected"
+      session=$(zellij ls 2>/dev/null | fzf --exit-0 --ansi --height 40% --header 'Enter to open session:') && zellij attach "${session%% *}" || echo "No sessions selected"
    fi
 }
 function zr () { 
