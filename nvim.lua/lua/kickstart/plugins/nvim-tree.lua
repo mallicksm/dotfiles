57f535f8ff978721d719e3d40f31b87df03b26c9
soummya <soummya@aeva.ai>
2024-03-20 08:48:12 -0700

    added indent_line and nvim-tree

--- nvim.lua/lua/kickstart/plugins/nvim-tree.lua
+++ nvim.lua/lua/kickstart/plugins/nvim-tree.lua
@@ -1,32 +1,50 @@
 return {
   { -- Useful plugin to show you pending keybinds.
-  'nvim-tree/nvim-tree.lua',
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-  -- disable netrw at the very start of your init.lua
-  vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
+    'nvim-tree/nvim-tree.lua',
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      -- disable netrw at the very start of your init.lua
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
 
-  -- optionally enable 24-bit colour
-  vim.opt.termguicolors = true
-  require("nvim-tree").setup({
-    sort = {
-      sorter = "case_sensitive",
-    },
-    view = {
-      width = function()
-        return math.floor(vim.api.nvim_win_get_width(0) / 2.8)
-      end,
-    },
-    filters = {
-      dotfiles = true,
-    },
-  })
-  end,
+      -- optionally enable 24-bit colour
+      vim.opt.termguicolors = true
+      local opt = {
+        sort = {
+          sorter = "filetype",
+        },
+        view = {
+          width = function()
+            return math.floor(vim.api.nvim_win_get_width(0) / 2.8)
+          end,
+          signcolumn = "auto",
+        },
+        filters = {
+          dotfiles = true,
+        },
+        git = {
+           timeout = 500,
+        },
+        renderer = {
+          add_trailing = false,
+          indent_width = 4,
+          indent_markers = {
+            enable = true,
+            icons = {
+              edge = "│",
+              item = "│",
+              corner = "│",
+              bottom = " ",
+            },
+          },
+        },
+      }
+      require("nvim-tree").setup(opt)
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
