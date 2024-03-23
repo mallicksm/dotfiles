44fb06ea198033227051ce1e011b7eea794de8ca
soummya <soummya@aeva.ai>
2024-03-23 15:56:24 -0700

    fixed nvim-tree look and file to match nerdtree

--- nvim.lua/lua/kickstart/plugins/nvim-tree.lua
+++ nvim.lua/lua/kickstart/plugins/nvim-tree.lua
@@ -13,13 +13,35 @@ return {
 
       -- optionally enable 24-bit colour
       vim.opt.termguicolors = true
+
+      -- set some keymaps within tree
+      local function my_on_attach(bufnr)
+        local api = require "nvim-tree.api"
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- custom mappings
+        vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
+        vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+        vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
+        vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+      end
+
       local opt = {
+        on_attach = my_on_attach,
+        sync_root_with_cwd = true,
+        hijack_cursor = true,
+        reload_on_bufenter = true,
         sort = {
           sorter = "filetype",
         },
         view = {
           width = function()
-            return math.floor(vim.api.nvim_win_get_width(0) / 2.8)
+            return math.floor(vim.api.nvim_win_get_width(0) / 2.5)
           end,
           signcolumn = "auto",
         },
@@ -27,11 +49,22 @@ return {
           dotfiles = true,
         },
         git = {
-           timeout = 500,
+          timeout = 500,
+        },
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+          ignore_list = { "help" },
+        },
+        help = {
+          sort_by = "desc"
         },
         renderer = {
-          add_trailing = false,
+          root_folder_label = function(path)
+            return "tree: " .. vim.fn.fnamemodify(path, ":~")
+          end,
           indent_width = 4,
+          highlight_opened_files = "name",
           indent_markers = {
             enable = true,
             icons = {
