274edc5a9d5a34ebb38ee8d040cb782c670694e5
soummya <soummya@aeva.ai>
2024-03-24 16:01:57 -0700

    fixed multi select sh snippet

--- snippets/lua_snippets/sh.lua
+++ snippets/lua_snippets/sh.lua
@@ -1,5 +1,8 @@
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local t = ls.text_node
 return {
-	s("btrig", t("also loaded!!"))
-}, {
+	s("btrig", t("also loaded!!")),
 	s("autotrig", t("autotriggered, if enabled"))
 }
