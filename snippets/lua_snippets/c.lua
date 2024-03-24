853774b9ab44b8bd62e28910c697d084c097c547
soummya <soummya@aeva.ai>
2024-03-24 15:22:21 -0700

    added two triggers

--- snippets/lua_snippets/c.lua
+++ snippets/lua_snippets/c.lua
@@ -2,9 +2,15 @@ local ls = require("luasnip")
 -- some shorthands...
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
 
 return {
-	s("ctrig", t("also loaded!!"))
-}, {
-	s("autotrig", t("autotriggered, if enabled"))
+	s("cheader", {
+		t({ "also", "loaded!!" }),
+		i(1),
+		t("also loaded!!")
+	}),
+	s("autotrig", {
+		t("autotriggered, if enabled")
+	})
 }
