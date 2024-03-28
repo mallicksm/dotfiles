834ad838043f1c6b96411251668ff0ec7d7d473d
soummya <soummya@aeva.ai>
2024-03-27 23:21:05 -0700

    fixed gitsigns color

--- nvim.lua/lua/kickstart/plugins/gitsigns.lua
+++ nvim.lua/lua/kickstart/plugins/gitsigns.lua
@@ -9,10 +9,9 @@ return {
     config = function()
       require('gitsigns').setup {
         signs = {
-          add          = { text = '+' },
-          change       = { text = '~' },
-          delete       = { text = '_' },
-          topdelete    = { text = '‾' },
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' }, topdelete = { text = '‾' },
           changedelete = { text = '~' },
           untracked    = { text = '┆' },
         },
@@ -20,6 +19,14 @@ return {
     end,
   },
   vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<cr>", { desc = "GitSigns: Toggle Blame" }),
-  vim.keymap.set("n", "<leader>gB", ":Gitsigns blame_line<cr>", { desc = "GitSigns: Blame line" })
+  vim.keymap.set("n", "<leader>gw", ":Gitsigns toggle_word_diff<cr>", { desc = "GitSigns: Toggle Word diff" }),
+  vim.keymap.set("n", "<leader>gn", ":Gitsigns toggle_numhl<cr>", { desc = "GitSigns: Toggle number highlight" }),
+  vim.keymap.set("n", "<leader>gd", ":Gitsigns toggle_deleted<cr>", { desc = "GitSigns: Toggle deleted lines" }),
+  vim.keymap.set("n", "<leader>gB", ":Gitsigns blame_line<cr>", { desc = "GitSigns: Blame line" }),
+  vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<cr>", { desc = "GitSigns: Hunk: next" }),
+  vim.keymap.set("n", "<leader>gp", ":Gitsigns previous_hunk<cr>", { desc = "GitSigns: Hunk: previous" }),
+  vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<cr>", { desc = "GitSigns: Hunk: stage" }),
+  vim.keymap.set("n", "<leader>gU", ":Gitsigns undo_stage_hunk<cr>", { desc = "GitSigns: Hunk: Unstage" }),
+  vim.keymap.set("n", "<leader>gR", ":Gitsigns reset_hunk<cr>", { desc = "GitSigns: Hunk: Reset" }),
 }
 -- vim: ts=2 sts=2 sw=2 et
--- nvim.lua/lua/kickstart/plugins/gruvbox.lua
+++ nvim.lua/lua/kickstart/plugins/gruvbox.lua
@@ -1,19 +1,26 @@
 return {
-  { -- You can easily change to a different colorscheme.
+  {
+    -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'https://github.com/ellisonleao/gruvbox.nvim',
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = true,   -- make sure to load this before all the other start plugins
-    init = function()
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    opts = {
+      terminal_colors = true,
+      palette_overrides = {
+        dark2 = "#6e6967",
+      }
+    },
+    config = function(_, opts)       -- make sure to load this before all the other start plugins
+      require("gruvbox").setup(opts) -- calling setup is optional
       -- Load the colorscheme here.
       vim.cmd.colorscheme 'gruvbox'
 
       -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
-  },
+  }
 }
 -- vim: ts=2 sts=2 sw=2 et
