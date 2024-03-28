b96d2e3f5276818540d22f9e4b66fc4c65473300
soummya <soummya@aeva.ai>
2024-03-28 12:15:33 -0700

    cleanup with lspconfig

--- nvim.lua/lua/kickstart/plugins/tokyodark.lua
+++ nvim.lua/lua/kickstart/plugins/tokyodark.lua
@@ -5,7 +5,7 @@ return {
       -- custom options here
     },
     config = function(_, opts)
-      require("tokyodark").setup(opts)   -- calling setup is optional
+      require("tokyodark").setup(opts) -- calling setup is optional
       vim.cmd [[colorscheme tokyodark]]
     end,
   }
