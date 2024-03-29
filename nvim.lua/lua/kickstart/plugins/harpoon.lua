327cce2e632d1b0519326a0885fd3b49e864d7a5
soummya <soummya@aeva.ai>
2024-03-29 09:42:03 -0700

    configured comma (,)

--- nvim.lua/lua/kickstart/plugins/harpoon.lua
+++ nvim.lua/lua/kickstart/plugins/harpoon.lua
@@ -3,31 +3,45 @@ return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
+
     config = function()
       local harpoon = require('harpoon')
-      harpoon:setup({})
+      harpoon:setup({
+        settings = {
+          -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+          save_on_toggle = false,
+        }
+      })
+      harpoon:extend({
+        UI_CREATE = function(cx)
+          vim.keymap.set("n", "v", function()
+            harpoon.ui:select_menu_item({ vsplit = true })
+          end, { buffer = cx.bufnr })
 
-      -- basic telescope configuration
-      local conf = require("telescope.config").values
-      local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(file_paths, item.value)
-        end
+          vim.keymap.set("n", "s", function()
+            harpoon.ui:select_menu_item({ split = true })
+          end, { buffer = cx.bufnr })
 
-        require("telescope.pickers").new({}, {
-          prompt_title = "Harpoon",
-          finder = require("telescope.finders").new_table({
-            results = file_paths,
-          }),
-          previewer = conf.file_previewer({}),
-          sorter = conf.generic_sorter({}),
-        }):find()
-      end
+          vim.keymap.set("n", "t", function()
+            harpoon.ui:select_menu_item({ tabedit = true })
+          end, { buffer = cx.bufnr })
+        end,
+      })
 
-      vim.keymap.set("n", "<leader>Hl", function() toggle_telescope(harpoon:list()) end,
-        { desc = "Harpoon: Open list picker" })
-      vim.keymap.set("n", "<leader>Ha", function() harpoon:list():append() end, { desc = "Harpoon: Append list" })
+      -- harpoon management edit(e) and add(a)
+      vim.keymap.set("n", ",e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+        { desc = "Harpoon: Open list edit" })
+      vim.keymap.set("n", ",a", function() harpoon:list():append() end, { desc = "Harpoon: Append list" })
+
+      -- remember by position 4 files, left right down and up
+      vim.keymap.set("n", ",h", function() harpoon:list():select(1) end, { desc = "Harpoon: Select left" })
+      vim.keymap.set("n", ",l", function() harpoon:list():select(2) end, { desc = "Harpoon: Select right" })
+      vim.keymap.set("n", ",j", function() harpoon:list():select(3) end, { desc = "Harpoon: Select down" })
+      vim.keymap.set("n", ",k", function() harpoon:list():select(4) end, { desc = "Harpoon: Select up" })
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", ",p", function() harpoon:list():prev() end, { desc = "Harpoon: previous" })
+      vim.keymap.set("n", ",n", function() harpoon:list():next() end, { desc = "Harpoon: next" })
     end
   }
 }
