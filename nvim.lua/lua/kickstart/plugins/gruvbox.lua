139173fefe92433fe5b9a510c3df68103f6f5cac
soummya <soummya@aeva.ai>
2024-03-30 16:32:53 -0700

    removed flash and made zellij more usable

--- nvim.lua/lua/kickstart/plugins/gruvbox.lua
+++ nvim.lua/lua/kickstart/plugins/gruvbox.lua
@@ -17,8 +17,6 @@ return {
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'GitSignsCurrentLineBlame gui=bold'
-      vim.cmd.hi 'FlashMatch guifg=orange'
-      vim.cmd.hi 'FlashLabel guifg=white'
     end,
   }
 }
