3713c4f108fa6b93f766da33b840c188d5eba970
soummya <soummya@aeva.ai>
2024-03-31 11:33:39 -0700

    added help to telescope

--- nvim.lua/lua/kickstart/plugins/telescope.lua
+++ nvim.lua/lua/kickstart/plugins/telescope.lua
@@ -77,16 +77,11 @@ return {
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', ',e', builtin.find_files, { desc = 'Telescope: [S]earch [F]iles' })
-      vim.keymap.set('n', ',g', builtin.live_grep, { desc = 'Telescope: [S]earch by [G]rep' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -99,10 +94,28 @@ return {
 
       -- Also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
+      vim.keymap.set('n', ',e', function()
+        builtin.find_files {
+          prompt_title = 'Find Files (<esc> to quit)'
+        }
+      end, { desc = 'Telescope: [S]earch [F]iles' })
+
+      vim.keymap.set('n', ',g', function()
+        builtin.live_grep {
+          prompt_title = 'Live grep (<esc> to quit)'
+        }
+      end, { desc = 'Telescope: [S]earch by [G]rep' })
+
+      vim.keymap.set('n', '<leader><leader>', function()
+        builtin.buffers {
+          prompt_title = 'Switch Buffers (<esc> to quit)'
+        }
+      end, { desc = '[ ] Find existing buffers' })
+
       vim.keymap.set('n', '<leader>s/', function()
         builtin.live_grep {
           grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
+          prompt_title = 'Live Grep in Open Files <ESC> to quit',
         }
       end, { desc = '[S]earch [/] in Open Files' })
 
