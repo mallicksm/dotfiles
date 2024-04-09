bf00fcf7ee64527f55e5867290f3b6461a0852a0
soummya <soummya@aeva.ai>
2024-04-09 09:37:22 -0700

    fixed modpath by putting in bash_snippets which must be sourced first

--- bash_functions.sh
+++ bash_functions.sh
@@ -9,36 +9,10 @@
 # Description: basic functions for shell
 #
 #===============================================================================
-function modpath () {
-   loc=$1
-   cmd=${2:-"d"}
-   var=${3:-PATH}
-   eval PATHv=\$$var    # set PATHv to the var reference (double indirection)
-   NEW_PATH=${PATHv/#"$loc:"}         #    Begining
-   NEW_PATH=${NEW_PATH/#"$loc"}       #    Solo
-   NEW_PATH=${NEW_PATH/%":$loc"}      #    Ending
-   NEW_PATH=${NEW_PATH//":$loc:"/:}   #    Multiple middles
-
-   if [ $cmd = "d" ]; then
-      export ${var}=$NEW_PATH
-   elif [ $cmd = "b" ]; then
-      if [ -z $NEW_PATH ]; then
-         export ${var}=$loc   # old PATH empty
-      else
-         export ${var}=$loc:$NEW_PATH
-      fi
-   elif [ $cmd = "a" ]; then
-      if [ -z $NEW_PATH ]; then
-         export ${var}=$loc   # old PATH empty
-      else
-         export ${var}=$NEW_PATH:$loc
-      fi
-   else
-      echo "usage: modpath [location] [d elete|b efore|a fter] [VARIABLE]"
-   fi
-}
-modpath ~/.local/bin b
 for file in $(command ls ~/dotfiles/utils/bash_*.sh); do
+   if [[ $file =~ "bash_snippets.sh" ]]; then 
+      continue
+   fi
    source $file
 done
 #-------------------------------------------------------------------------------
--- utils/bash_snippets.sh
+++ utils/bash_snippets.sh
@@ -181,3 +181,31 @@ function get_clip() {
    command gvim $file -T dumb --noplugin -n -es -c 'set nomore' +'normal "*P' +'wq'
    cat $file
 }
+function modpath () {
+   loc=$1
+   cmd=${2:-"d"}
+   var=${3:-PATH}
+   eval PATHv=\$$var    # set PATHv to the var reference (double indirection)
+   NEW_PATH=${PATHv/#"$loc:"}         #    Begining
+   NEW_PATH=${NEW_PATH/#"$loc"}       #    Solo
+   NEW_PATH=${NEW_PATH/%":$loc"}      #    Ending
+   NEW_PATH=${NEW_PATH//":$loc:"/:}   #    Multiple middles
+
+   if [ $cmd = "d" ]; then
+      export ${var}=$NEW_PATH
+   elif [ $cmd = "b" ]; then
+      if [ -z $NEW_PATH ]; then
+         export ${var}=$loc   # old PATH empty
+      else
+         export ${var}=$loc:$NEW_PATH
+      fi
+   elif [ $cmd = "a" ]; then
+      if [ -z $NEW_PATH ]; then
+         export ${var}=$loc   # old PATH empty
+      else
+         export ${var}=$NEW_PATH:$loc
+      fi
+   else
+      echo "usage: modpath [location] [d elete|b efore|a fter] [VARIABLE]"
+   fi
+}
