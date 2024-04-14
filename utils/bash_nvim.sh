2347f2491eefb5cffbbd5fcc8b46173ecbf31db7
soummya <soummya@aeva.ai>
2024-04-14 11:51:32 -0700

    added docs and svim for old nonlsp nvim

--- utils/bash_nvim.sh
+++ utils/bash_nvim.sh
@@ -7,3 +7,12 @@ function vi () {
    export XDG_CONFIG_HOME=$_XDG_CONFIG_HOME
    export NVIM_APPNAME=$_NVIM_APPNAME
 }
+function svim () {
+   _XDG_CONFIG_HOME=$XDG_CONFIG_HOME
+   _NVIM_APPNAME=$NVIM_APPNAME
+   export XDG_CONFIG_HOME=~/dotfiles/
+   export NVIM_APPNAME=nvim
+   nvim -p "$@"
+   export XDG_CONFIG_HOME=$_XDG_CONFIG_HOME
+   export NVIM_APPNAME=$_NVIM_APPNAME
+}
