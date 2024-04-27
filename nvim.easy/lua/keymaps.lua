841dce708400deba509ad8f1d52d5bd8d0e6245e
mallicksm <mallicksm@gmail.com>
2024-04-26 17:44:21 -0700

    removed junk keymaps

--- nvim.easy/lua/keymaps.lua
+++ nvim.easy/lua/keymaps.lua
@@ -5,10 +5,6 @@
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-
 -- Rapid quit keymap
 vim.keymap.set('n', 'gq', 'q', { desc = 'Record a macro' })
 vim.keymap.set('n', 'q', '<cmd>q<CR>', { desc = 'Quit if no change' })
@@ -17,10 +13,10 @@ vim.keymap.set('n', '<leader>x', '<cmd>wqa!<CR>', { desc = 'Write quit all and a
 
 -- Keybinds to make split navigation easier.
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<leader>h', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<leader>l', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<leader>j', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<leader>k', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>h', '<C-w><C-h>', { desc = 'Nav: left window' })
+vim.keymap.set('n', '<leader>l', '<C-w><C-l>', { desc = 'Nav: right window' })
+vim.keymap.set('n', '<leader>j', '<C-w><C-j>', { desc = 'Nav: lower window' })
+vim.keymap.set('n', '<leader>k', '<C-w><C-k>', { desc = 'Nav: upper window' })
 
 -- [[ Basic Filetypes ]]
 -- add custom similar filetypes
