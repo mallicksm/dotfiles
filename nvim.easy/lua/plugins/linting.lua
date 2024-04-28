5bc8b46e17c28df727ad44f8213aebb69c67e6d8
mallicksm <mallicksm@gmail.com>
2024-04-28 10:49:57 -0700

    added linting

---
+++ nvim.easy/lua/plugins/linting.lua
@@ -0,0 +1,13 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("lint").linters_by_ft = {
+			c = { "clangtidy" },
+		}
+		vim.keymap.set("n", "<leader>ml", function()
+			require("lint").try_lint()
+		end, { desc = "Lnt: Trigger linting" })
+	end,
+}
+-- vim: ts=3 sts=3 sw=3 et
