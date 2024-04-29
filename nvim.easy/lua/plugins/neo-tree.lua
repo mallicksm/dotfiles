e01dfe847c707e1b02450ebcb854da256dc16caa
mallicksm <mallicksm@gmail.com>
2024-04-28 22:47:04 -0700

    synced up neo-tree to nvim.lua

--- nvim.easy/lua/plugins/neo-tree.lua
+++ nvim.easy/lua/plugins/neo-tree.lua
@@ -3,11 +3,71 @@ return {
    branch = 'v3.x',
    dependencies = {
       'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
+      {
+         'nvim-tree/nvim-web-devicons',
+         config = function()
+            require('nvim-web-devicons').setup({
+               override_by_extension = {
+                  ['f'] = {
+                     icon = '',
+                     color = '#4285f4',
+                     name = 'f',
+                  },
+                  ['tdf'] = {
+                     icon = '󰃰',
+                     color = '#89e051',
+                     name = 'tdf',
+                  },
+                  ['cmm'] = {
+                     icon = '⚒️',
+                     color = '#89e051',
+                     name = 'cmm',
+                  },
+                  ['qel'] = {
+                     icon = '󰛓',
+                     color = '#e37933',
+                     name = 'qel',
+                  },
+                  ['bash'] = {
+                     icon = '󱆃',
+                     color = '#89e051',
+                     cterm_color = '113',
+                     name = 'bash',
+                  },
+               },
+            })
+         end,
+      },
       'MunifTanjim/nui.nvim',
-      '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+      '3rd/image.nvim',
    },
    config = function()
+      require('neo-tree').setup({
+         window = {
+            width = 40,
+            mappings = {
+               ['s'] = 'open_split',
+               ['v'] = 'open_vsplit',
+               ['Y'] = {
+                  function(state)
+                     local node = state.tree:get_node()
+                     local path = node:get_id()
+                     print('Copied to clipboard: ' .. path)
+                     vim.fn.setreg('*', path)
+                  end,
+                  desc = 'Copy Path to Clipboard',
+               },
+            },
+         },
+         filesystem = {
+            window = {
+               mappings = {
+                  ['u'] = 'navigate_up',
+                  ['C'] = 'set_root',
+               },
+            },
+         },
+      })
       vim.keymap.set('n', '<leader>e', ':Neotree reveal_force_cwd toggle<cr>', { desc = 'Neo-tree: File browser toggle' })
    end,
 }
