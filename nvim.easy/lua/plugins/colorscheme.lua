b3a44719ba6ff407a0c089e3074e0b20f01828ab
mallicksm <mallicksm@gmail.com>
2024-04-28 21:27:04 -0700

    went back to gruvbox

--- nvim.easy/lua/plugins/colorscheme.lua
+++ nvim.easy/lua/plugins/colorscheme.lua
@@ -1,9 +1,19 @@
 return {
+   --[[
    'catppuccin/nvim',
    name = 'catppuccin',
    priority = 1000,
    config = function()
       vim.cmd.colorscheme('catppuccin-mocha')
+   end,
+   --]]
+   'ellisonleao/gruvbox.nvim',
+   priority = 1000,
+   config = function()
+      require('gruvbox').setup({
+         terminal_colors = true,
+      })
+      vim.cmd.colorscheme('gruvbox')
    end,
 }
 -- vim: ts=3 sts=3 sw=3 et
---
+++ nvim.easy/lua/plugins/core_vim-sleuth.lua
@@ -0,0 +1,4 @@
+return {
+   'tpope/vim-sleuth',
+}
+-- vim: ts=3 sts=3 sw=3 et
