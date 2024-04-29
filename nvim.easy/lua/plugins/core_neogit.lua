26b45c48e5b5820efaa29094fce62822e94f43c8
mallicksm <mallicksm@gmail.com>
2024-04-28 19:00:29 -0700

    added neogit

---
+++ nvim.easy/lua/plugins/core_neogit.lua
@@ -0,0 +1,28 @@
+return {
+   {
+      'NeogitOrg/neogit',
+      dependencies = {
+         'nvim-lua/plenary.nvim',
+         'sindrets/diffview.nvim',
+         'nvim-telescope/telescope.nvim',
+      },
+      config = function()
+         require('neogit').setup({
+            integrations = {
+               diffview = true,
+               telescope = true,
+            },
+            graph_style = 'unicode',
+            console_timeout = 5000,
+         })
+         vim.keymap.set('n', '<leader>gg', ':Neogit kind=auto<cr>', { desc = 'Neogit: Git status CLI' })
+         vim.keymap.set('n', '<leader>gd', function()
+            if next(require('diffview.lib').views) == nil then
+               vim.cmd('DiffviewOpen -uno')
+            else
+               vim.cmd('DiffviewClose')
+            end
+         end, { desc = 'Diffview: toggle' })
+      end,
+   },
+}
