905c1e37d9ead9001f8bcf910d5e472548c20200
soummya <soummya@aeva.ai>
2024-05-01 15:05:09 -0700

    added lsp info to lualine

--- nvim.easy/lua/plugins/core_lualine.lua
+++ nvim.easy/lua/plugins/core_lualine.lua
@@ -1,9 +1,24 @@
 return {
    'nvim-lualine/lualine.nvim',
    config = function()
+      -- LSP clients attached to buffer
+      local clients_lsp = function()
+         local bufnr = vim.api.nvim_get_current_buf()
+         local clients = vim.lsp.get_clients({ buffer = bufnr })
+         if next(clients) == nil then
+            return ''
+         end
+         local c = {}
+         for _, client in pairs(clients) do
+            table.insert(c, client.name)
+         end
+         return '\u{f085} ' .. table.concat(c, '\u{2016}')
+      end
+
       require('lualine').setup({
          options = {
             theme = 'dracula',
+            component_separators = { left = '\u{2016}', right = '\u{2016}' },
             disabled_filetypes = {
                statusline = {
                   "neo-tree",
@@ -13,7 +28,7 @@ return {
             },
          },
          sections = {
-            lualine_x = { "fileformat", "filetype" }
+            lualine_x = { clients_lsp, "fileformat", "filetype" },
          },
          inactive_sections = {
             lualine_c = { { "filename", path = 1 } },
