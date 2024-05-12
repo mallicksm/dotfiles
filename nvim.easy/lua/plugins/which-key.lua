420a5b61bc0052cac1be868d55ba336aad3aa5aa
soummya <soummya@aeva.ai>
2024-05-11 23:05:40 -0700

    added which_key_ignore

--- nvim.easy/lua/plugins/which-key.lua
+++ nvim.easy/lua/plugins/which-key.lua
@@ -3,6 +3,10 @@ return {
    event = 'VimEnter',
    config = function()
       require('which-key').setup()
+      require('which-key').register {
+         ['<leader>d'] = { name = '[D]ap', _ = 'which_key_ignore' },
+         ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+      }
    end,
 }
 -- vim: ts=3 sts=3 sw=3 et
