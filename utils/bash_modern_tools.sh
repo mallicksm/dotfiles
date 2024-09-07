871b30011012158b12520ac8825ee1bb6d7a6b41
soummya <soummya@aeva.ai>
2024-09-07 13:45:05 -0700

    checkin

--- utils/bash_modern_tools.sh
+++ utils/bash_modern_tools.sh
@@ -12,21 +12,22 @@ unalias ll 2> /dev/null
 export LS_COLORS="*.c=31:*.h=32;;01:*.svh=32:*.sv=33;;01:*.tcl=34;;01:*.f=35:*.qel=34:*.v=33:*.tdf=36:*.cmm=37"
 function l() {
    if command -v exa >/dev/null ; then
-      command exa --color=always --icons --sort=Ext --group-directories-first "$@"
+      command exa --color=always --icons --sort=Ext --group-directories-first --time-style=long-iso "$@"
    else
       command ls -Fslr --color=auto "$@"
    fi
 }
 function ll() {
    if command -v exa >/dev/null ; then
-      command exa --long --header --group -s modified "$@"
+      command exa --long --header -s modified --time-style=long-iso "$@"
    else
       command ls -Fslr --color=auto "$@"
    fi
 }
 function lsd() {
    if command -v exa >/dev/null ; then
-      command exa --long --header --group -s modified -D "$@"
+      args=$@*/
+      command exa --long --header -s modified -d --time-style=long-iso $args
    else
       command ls -Fslr --color=auto "$@"
    fi
@@ -34,7 +35,7 @@ function lsd() {
 unalias la 2> /dev/null
 function la() {
    if command -v exa >/dev/null ; then
-      command exa --long --header --group -s modified -a "$@"
+      command exa --long --header -s modified -a --time-style=long-iso "$@"
    else
       command ls -Fslra --color=auto "$@"
    fi
