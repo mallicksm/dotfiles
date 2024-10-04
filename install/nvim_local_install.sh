dccfb862872fe50ccd085b20b86180316cac64e0
soummya <soummya@aeva.ai>
2024-10-04 10:26:25 -0700

    neovim install from stable, nightly very unstable

--- install/nvim_local_install.sh
+++ install/nvim_local_install.sh
@@ -10,11 +10,10 @@
 #
 #===============================================================================
 source ~/dotfiles/utils/bash_snippets.sh 2>/dev/null
-
 # Latest glibc
 src=https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
 # Unsupported builds for legacy glibc
-src=https://github.com/neovim/neovim-releases/releases/download/nightly/nvim-linux64.tar.gz
+src=https://github.com/neovim/neovim-releases/releases/download/stable/nvim-linux64.tar.gz
 dst=/opt/si/emu/users/$USER/tools/nvim_$(date +"%b%d_%I%M%P")
 command mkdir -pv $dst && command cd $dst && command rm -rf $(pwd)/../nvim_latest && command ln -sf $(pwd) $(pwd)/../nvim_latest
 download $src $(basename $src)
