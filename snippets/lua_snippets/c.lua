3360c5b87f0fa4c643f4cf0dd802ef94b0dfb823
soummya <soummya@aeva.ai>
2024-12-12 14:24:09 -0800

    cleanup

--- snippets/lua_snippets/c.lua
+++ snippets/lua_snippets/c.lua
@@ -15,9 +15,7 @@ ls.add_snippets("c", {
          t("void"),                  -- Option for void
          t("int argc, char *argv[]") -- Option for argc, argv
       }),
-      body = i(0)                    -- Placeholder for the body of the function
-   }, {
-      name = "Main function with 2 argument styles"
+      body = i(2)                    -- Placeholder for the body of the function
    })),
 
    -- Single 'if' snippet with options for 'else' and 'else if'
@@ -42,14 +40,15 @@ ls.add_snippets("c", {
          ]], { cond = i(1, "1"), body = i(2) }) -- 'else if' block
       })
    })),
+
+   -- Single '#if' snippet with options for 'else' and 'else if'
    s("#if", fmt([[
-   <cond>
-   <body>
-   <else_part>
-   #endif
+      <cond>
+      <body>
+      <else_part>
+      #endif
    ]], {
 
-      -- Single '#if' snippet with options for 'else' and 'else if'
       cond = c(2, {
          fmt("#ifdef <cond>", { cond = i(1, "FEATURE_A") }),                     -- Option 1: #ifdef FEATURE_A
          fmt("#ifndef <cond>", { cond = i(1, "FEATURE_A") }),                    -- Option 1: #ifdef FEATURE_A
@@ -57,7 +56,7 @@ ls.add_snippets("c", {
          fmt("#if (<cond>)", { cond = i(1, "FEATURE_A > 2") }),                  -- Option 3: #if (FEATURE_A > 2)
          fmt("#if (<cond>)", { cond = i(1, "FEATURE_A > 2 || FEATURE_B > 3") }), -- Option 4: #if (FEATURE_A > 2 || FEATURE_B > 3)
       }),
-      body = i(3, "// code"),                                                 -- Placeholder for the conditional block
+      body = i(3, "// code"),                                                    -- Placeholder for the conditional block
       else_part = c(1, {
          t(""),                                                                  -- No #else or #elif block
          fmt([[
