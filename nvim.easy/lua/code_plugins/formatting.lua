40c0ba64611474eadf5156877c3e3d01bb6ef828
soummya <soummya@aeva.ai>
2025-04-18 12:03:33 -0700

    mods

--- nvim.easy/lua/code_plugins/formatting.lua
+++ nvim.easy/lua/code_plugins/formatting.lua
@@ -13,6 +13,16 @@ return {
                   command = 'verible-verilog-format',
                   args = { "--port_declarations_indentation=indent", "--port_declarations_alignment=align", "--indentation_spaces=3", "$FILENAME" },
                },
+               black = {
+                  command = "black",
+                      args = {
+                        "--config",
+                        vim.fn.expand("$HOME") .. "/dotfiles/formatters/py-format.toml",
+                        "--quiet",
+                        "-"
+                  },
+                  stdin = true,
+               }
             },
             formatters_by_ft = {
                lua = { 'stylua' },
