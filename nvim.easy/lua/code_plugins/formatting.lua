96ecefbf3a6dbda08d033285ed34b90179fa4558
soummya <soummya@aeva.ai>
2025-04-19 10:50:35 -0700

    mods

--- nvim.easy/lua/code_plugins/formatting.lua
+++ nvim.easy/lua/code_plugins/formatting.lua
@@ -7,29 +7,29 @@ return {
             formatters = {
                clang_format = {
                   command = 'clang-format',
-                  args = { '--style=file:' .. vim.fn.expand("$HOME") .. '/dotfiles/formatters/clang-format' },
+                  args = { '--style=file:' .. vim.fn.expand('$HOME') .. '/dotfiles/formatters/clang-format' },
                },
                verible_verilog_format = {
                   command = 'verible-verilog-format',
-                  args = { "--port_declarations_indentation=indent", "--port_declarations_alignment=align", "--indentation_spaces=3", "$FILENAME" },
+                  args = { '--port_declarations_indentation=indent', '--port_declarations_alignment=align', '--indentation_spaces=3', '$FILENAME' },
                },
                black = {
-                  command = "black",
-                      args = {
-                        "--config",
-                        vim.fn.expand("$HOME") .. "/dotfiles/formatters/py-format.toml",
-                        "--quiet",
-                        "-"
+                  command = 'black',
+                  args = {
+                     '--config',
+                     vim.fn.expand('$HOME') .. '/dotfiles/formatters/py-format.toml',
+                     '--quiet',
+                     '-',
                   },
                   stdin = true,
-               }
+               },
             },
             formatters_by_ft = {
-               lua = { 'stylua' },
-               json = { 'prettier' },
-               python = {"black"},
-               c = { 'clang_format' },
-               verilog_systemverilog = { 'verible_verilog_format' },
+               lua = { 'stylua' }, -- brew install stylua
+               json = { 'prettier' }, -- npm install prettier
+               python = { 'black' }, -- pip install black
+               c = { 'clang_format' }, -- gcc13 + clang11
+               verilog_systemverilog = { 'verible_verilog_format' }, -- https://github.com/chipsalliance/verible/releases
             },
             notify_on_error = false,
          })
