e6d36cf9d87cc7393b13979ae4c88a830f366c17
mallicksm <mallicksm@gmail.com>
2025-04-28 18:21:31 -0700

    mods

--- nvim.easy/lua/core_plugins/mini.lua
+++ nvim.easy/lua/core_plugins/mini.lua
@@ -27,8 +27,8 @@ return {
          require('mini.ai').setup({
             custom_textobjects = {
                B = require('mini.extra').gen_ai_spec.buffer(),
-               L = require('mini.extra').gen_ai_spec.line()
-            }
+               L = require('mini.extra').gen_ai_spec.line(),
+            },
          })
 
          -------------------------------------------------------
@@ -41,12 +41,12 @@ return {
          ----------------------------------------------------
          require('mini.pairs').setup({
             mappings = {
-               ["`"] = false,
+               ['`'] = false,
             },
             disable_filetypes = {
                'TelescopePrompt',
                'NvimTree',
-               'neo-tree'
+               'neo-tree',
             },
          })
 
@@ -55,98 +55,20 @@ return {
          ------------------------------------------------
          require('mini.comment').setup()
 
-         ------------------------------------------------
-         -- <leader>K for more info on cWORD MiniStarter-example-config
-         ------------------------------------------------
-         local builtin = require("telescope.builtin")
-         local harpoon = require('harpoon')
-         require('mini.starter').setup({
-            evaluate_single = true,
-            -- https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=LAZYVIM
-            header = table.concat({
-               "███╗   ██╗██╗   ██╗██╗███╗   ███╗   ███████╗ █████╗ ███████╗██╗   ██╗",
-               "████╗  ██║██║   ██║██║████╗ ████║   ██╔════╝██╔══██╗██╔════╝╚██╗ ██╔╝",
-               "██╔██╗ ██║██║   ██║██║██╔████╔██║   █████╗  ███████║███████╗ ╚████╔╝ ",
-               "██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║   ██╔══╝  ██╔══██║╚════██║  ╚██╔╝  ",
-               "██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║██╗███████╗██║  ██║███████║   ██║   ",
-               "╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝╚═╝╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ",
-            }, "\n"
-            ),
-            items = {
-               {
-                  name = 'recent Files',
-                  action = function()
-                     builtin.oldfiles({
-                        prompt_title = "Recent Files (<esc> to quit)"
-                     })
-                  end,
-                  section = 'Search'
-               },
-               {
-                  name = 'find Files',
-                  action = function()
-                     builtin.find_files({
-                        prompt_title = "Find Files (<esc> to quit)"
-                     })
-                  end,
-                  section = 'Search'
-               },
-               {
-                  name = 'live grep',
-                  action = function()
-                     builtin.live_grep({
-                        prompt_title = "LiveGrep Files (<esc> to quit)"
-                     })
-                  end,
-                  section = 'Search'
-               },
-               {
-                  name = 'harpoon',
-                  action = function()
-                     harpoon.ui:toggle_quick_menu(harpoon:list())
-                  end,
-                  section = 'Search'
-               },
-               {
-                  name = 'git Status',
-                  action = 'Telescope git_status',
-                  section = 'Git'
-               },
-               {
-                  name = 'lazygit',
-                  action = function() require("snacks").lazygit() end,
-                  section = 'Git'
-               },
-               {
-                  name = "nvim config Files",
-                  action = function()
-                     require("telescope.builtin").find_files({ cwd = "~/dotfiles/nvim.easy" })
-                  end,
-                  section = "Config",
-               },
-               { name = 'new File', action = 'enew', section = 'Actions' },
-               { name = 'quit',     action = 'qa',   section = 'Actions' },
-            },
-            content_hooks = {
-               require('mini.starter').gen_hook.adding_bullet('● '), -- Adds ● before each item
-               require('mini.starter').gen_hook.aligning('center', 'center') -- Centers items on the screen
-            },
-         })
          ---------------------------------------------------
          -- <leader>K for more info on cWORD mini.hipatterns
          ---------------------------------------------------
          require('mini.hipatterns').setup({
             highlighters = {
                -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-               fixme     = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-               hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
-               todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-               note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+               fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+               hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+               todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+               note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
 
                -- Highlight hex color strings (`#rrggbb`) using that color
                hex_color = require('mini.hipatterns').gen_highlighter.hex_color(),
             },
-
          })
          --  Check out: https://github.com/echasnovski/mini.nvim
       end,
