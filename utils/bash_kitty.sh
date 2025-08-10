f0e66809bdad5884e6a7d6633190350cac60d69e
smallick <smallick@upscaleai.com>
2025-08-10 16:46:25 -0700

    mods

--- utils/bash_kitty.sh
+++ utils/bash_kitty.sh
@@ -1,1 +1,3 @@
 export KITTY_CONFIG_DIRECTORY=~/dotfiles/initrc/kitty
+# in mac exec this command to fix gui launch of kitty
+# launchctl setenv KITTY_CONFIG_DIRECTORY /Users/smallick/dotfiles/initrc/kitty
