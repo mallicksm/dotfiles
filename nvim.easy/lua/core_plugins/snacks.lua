30abf8582b5195375f2176fd710698ce9fd1779d
smallick <smallick@upscaleai.com>
2026-05-02 20:29:30 -0700

    image previewer

--- nvim.easy/lua/core_plugins/snacks.lua
+++ nvim.easy/lua/core_plugins/snacks.lua
@@ -1,5 +1,5 @@
 return {
-   "folke/snacks.nvim",
+   'folke/snacks.nvim',
    priority = 1000,
    lazy = false,
    opts = {
@@ -7,11 +7,24 @@ return {
          win = {
             style = 'float',
          },
-      }
+      },
+      image = {
+         enabled = true,
+         doc = {
+            enabled = true,
+            inline = true,
+            float = true,
+            max_width = 80,
+            max_height = 40,
+         },
+         convert = {
+            magick = { 'magick' },
+         },
+      },
    },
    config = function(_, opts)
       require('snacks').setup(opts)
-      _G.Snacks = require("snacks")
+      _G.Snacks = require('snacks')
    end,
 }
 -- vim: ts=3 sts=3 sw=3 et
